4d0eaae501e8fa31901b1bc6ac363f9d2f520abb
margoam <mozheliukrita@gmail.com>
2021-09-25 14:49:05 +0300

    Выделение вспомогательных методов и передача параметров в методы

--- testAddGroup.py
+++ testAddGroup.py
@@ -11,31 +11,31 @@ class TestAddGroup(unittest.TestCase):
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/group.php")
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_xpath("//*/text()[normalize-space(.)='']/parent::*").click()
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
 
     def open_group_creation(self, wd):
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def create_group(self, wd):
+    def create_group(self, wd, name, header, footer):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("test3")
+        wd.find_element_by_name("group_name").send_keys(name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("dsffsd")
+        wd.find_element_by_name("group_header").send_keys(header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("sggsdgs")
+        wd.find_element_by_name("group_footer").send_keys(footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
@@ -48,9 +48,9 @@ class TestAddGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)  # вызов метода open_home_page
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_group_creation(wd)
-        self.create_group(wd)
+        self.create_group(wd, name="test3", header="dsffsd", footer="sggsdgs")
         self.return_to_group_page(wd)
         self.logout(wd)
 
