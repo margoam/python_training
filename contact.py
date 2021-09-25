7d733c64ea661f01e3e7882db2a3fd589c0fb4cd
margoam <mozheliukrita@gmail.com>
2021-09-25 16:43:57 +0300

    Выделение класса

---
+++ contact.py
@@ -0,0 +1,13 @@
+class Contact:
+
+    def __init__(self, first_name, middle_name, last_name, address, home_number, mobile_number, birth_day,
+                       birth_month, birth_year):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.address = address
+        self.home_number = home_number
+        self.mobile_number = mobile_number
+        self.birth_day = birth_day
+        self.birth_month = birth_month
+        self.birth_year = birth_year
