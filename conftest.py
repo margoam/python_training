4e75ba91b41a8d718405c8509e2d6f074d4a7020
margoam <mozheliukrita@gmail.com>
2021-10-03 14:48:02 +0300

    Редактирование фикстуры - одна на всю сессию

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from fixture.application import Application
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")  # фикстура создается одна на всю сессию
 def app(request):
     fixture = Application()  # Инициализация фикстуры
     request.addfinalizer(fixture.destroy)  # Как должна быть разрушена фикстура
