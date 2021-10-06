20fdce5142379decb587e600882cc32afd0e3d11
margoam <mozheliukrita@gmail.com>
2021-10-06 18:36:01 +0300

    Добавление теста для редактирования имеющейся группы (исправление наименования функций)

--- fixture/group.py
+++ fixture/group.py
@@ -54,7 +54,6 @@ class GroupHelper:
         wd.find_element_by_name("update").click()
         self.return_to_group_page()
 
-
     def return_to_group_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
