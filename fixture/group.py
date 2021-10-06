1a91845d4357c125d2e4bad362ad9d928f5634bd
margoam <mozheliukrita@gmail.com>
2021-10-06 18:34:03 +0300

    Добавление теста для редактирования имеющейся группы

--- fixture/group.py
+++ fixture/group.py
@@ -35,6 +35,26 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
 
+    def edit(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # выбрать первую группу
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("edit").click()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group creation
+        wd.find_element_by_name("update").click()
+        self.return_to_group_page()
+
+
     def return_to_group_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
---
+++ test/test_modif_group.py
@@ -0,0 +1,7 @@
+from model.group import Group
+
+
+def test_add_group(app):  # Тестовый метод, принимающий фикстуру в качестве параметра
+    app.session.login(username="admin", password="secret")  # всп метод
+    app.group.edit(Group(name="edit_test3", header="edit_text1", footer="edit_text2"))  # всп метод
+    app.session.logout()  # всп метод
