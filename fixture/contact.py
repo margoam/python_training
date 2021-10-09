567fd057e0ecc8bdbbb8cdfc3ae0cc6bc33691d5
margoam <mozheliukrita@gmail.com>
2021-10-09 15:15:22 +0300

    Вынесение дублирующего кода для контактов в отдельные методы

--- fixture/contact.py
+++ fixture/contact.py
@@ -9,36 +9,84 @@ class ContactHelper:
         # добавление домашней страницы, на которой находятся созданные контакты
         wd.get("http://localhost/addressbook/index.php")
 
-    def create(self, contact):
-        wd = self.app.wd
-        # fill contact form
-        wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.first_name)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.last_name)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+    def date_of_birth(self, contact, wd):
+        wd.find_element_by_name("bday").click()
+        wd.find_element_by_name("byear").send_keys(contact.birth_day)
+        wd.find_element_by_name("bmonth").click()
+        wd.find_element_by_name("byear").send_keys(contact.birth_month)
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(contact.birth_year)
+
+    def address_first(self, contact, wd):
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
         wd.find_element_by_name("address").send_keys(contact.address)
+
+    def phone_number(self, contact, wd):
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
         wd.find_element_by_name("home").send_keys(contact.home_number)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
         wd.find_element_by_name("mobile").send_keys(contact.mobile_number)
-        wd.find_element_by_name("bday").click()
-        wd.find_element_by_name("bmonth").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.birth_year)
+
+    def nick_name(self, contact, wd):
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
+
+    def last_name(self, contact, wd):
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+
+    def middle_name(self, contact, wd):
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
+
+    def first_name(self, contact, wd):
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+
+    def text_notes(self, contact, wd):
+        wd.find_element_by_name("notes").click()
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys(contact.notes)
+
+    def second_address(self, contact, wd):
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(contact.address_sec)
+
+    def email(self, contact, wd):
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+
+    def title(self, contact, wd):
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contact.title)
+
+    def name_of_company(self, contact, wd):
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+
+    def create(self, contact):
+        wd = self.app.wd
+        # fill contact form
+        wd.find_element_by_link_text("add new").click()
+        self.first_name(contact, wd)
+        self.middle_name(contact, wd)
+        self.last_name(contact, wd)
+        self.nick_name(contact, wd)
+        self.address_first(contact, wd)
+        self.phone_number(contact, wd)
+        self.date_of_birth(contact, wd)
         # submit contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
@@ -57,21 +105,12 @@ class ContactHelper:
         # выбрать первый контакт
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contact.address_sec)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(contact.notes)
+        self.nick_name(contact, wd)
+        self.name_of_company(contact, wd)
+        self.title(contact, wd)
+        self.email(contact, wd)
+        self.second_address(contact, wd)
+        self.text_notes(contact, wd)
         wd.find_element_by_name("update").click()
 
     def return_to_home_page(self):
