36a6f96d643c0b17cdc2c4d2034b58d1382c87d9
margoam <mozheliukrita@gmail.com>
2021-10-09 20:06:16 +0300

    Убраны лишние параметры при создании контакта

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,6 +4,6 @@ from model.contact import Contact
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.create(Contact(first_name="Test_name", middle_name="Test_middle_name", last_name="Test_last_name",
-                               nick_name="", address="asdasdasd", home_number="123", mobile_number="131231231",
-                               birth_day="3", birth_month="March", birth_year="1996", email="", notes=""))
+                               address="asdasdasd", home_number="123", mobile_number="131231231",
+                               birth_day="3", birth_month="March", birth_year="1996"))
     app.session.logout()
