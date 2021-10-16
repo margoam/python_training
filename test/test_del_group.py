3f4b6aace83f3f2ccb21c084e2ca8dbe5dfeff22
margoam <mozheliukrita@gmail.com>
2021-10-16 19:28:24 +0300

    редактирование теста удаления группы

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -4,8 +4,8 @@ from model.group import Group
 def test_delete_first_group(app):  # Тестовый метод, принимающий фикстуру в качестве параметра
     if app.group.count() == 0:
         app.group.create(Group(name="test3", header="dsffsd", footer="sggsdgs"))
-    app.group.delete_first_group()
     old_groups = app.group.get_group_list()
+    app.group.delete_first_group()
     new_groups = app.group.get_group_list()
     assert len(old_groups) - 1 == len(new_groups)
 
