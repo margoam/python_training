ad5c0683734668bbd88235249dd5d731a0c05655
margoam <mozheliukrita@gmail.com>
2021-10-16 19:24:05 +0300

    реализация выгружаемой информации из приложения в виде списков для удаления и модификации групп и сравнение их

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -5,4 +5,7 @@ def test_delete_first_group(app):  # Тестовый метод, принима
     if app.group.count() == 0:
         app.group.create(Group(name="test3", header="dsffsd", footer="sggsdgs"))
     app.group.delete_first_group()
+    old_groups = app.group.get_group_list()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
 
--- test/test_modif_contact.py
+++ test/test_modif_contact.py
@@ -8,3 +8,6 @@ def test_edit_first_contact(app):
                                    birth_day="15", birth_month="July", birth_year="1993"))
     app.contact.edit(Contact(nick_name="test@@123", home_number="+234234243", birth_day="9", birth_month="June",
                              birth_year="1990", email="example@gmail.com", notes="text"))
+    old_groups = app.group.get_group_list()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
