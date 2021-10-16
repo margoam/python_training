f2a781e6acfafb4e66c0780d3554f4fea2e1cd0b
margoam <mozheliukrita@gmail.com>
2021-10-16 19:17:29 +0300

    реализация выгружаемой информации из приложения в виде списков для групп и сравнение их

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,4 +2,7 @@ from model.group import Group
 
 
 def test_add_group(app):  # Тестовый метод, принимающий фикстуру в качестве параметра
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="test3", header="dsffsd", footer="sggsdgs"))  # всп метод
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
