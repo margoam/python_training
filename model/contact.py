1e85c855aa1db874f48be065e5efd5a38ad6cf46
margoam <mozheliukrita@gmail.com>
2021-10-18 08:49:16 +0300

    Добавлены пробелы в repr

--- model/contact.py
+++ model/contact.py
@@ -21,7 +21,7 @@ class Contact:
         self.id = id
 
     def __repr__(self):
-        return "%s%s%s" % (self.id, self.first_name, self.last_name)
+        return "%s %s %s" % (self.id, self.first_name, self.last_name)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and (self.first_name is None or
