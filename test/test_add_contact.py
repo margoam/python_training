1c80c39fb6ff35dff964ed7dd81f4dd302bbf77d
margoam <mozheliukrita@gmail.com>
2021-10-25 23:06:09 +0300

    Добавлена параметризация тестов для создания групп и контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,11 +1,26 @@
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*7
+    return prefix + "".join([random.choice(symbols) for _ in range(random.randrange(maxlen))])
+
+
+list_of_month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
+'October', 'November', 'December']
+
+test_data = [Contact(first_name=random_string("first_name", 10), middle_name=random_string("middle_name", 10),
+                     last_name=random_string("last_name", 10), address=random_string("address", 20),
+                     home_number='+' + str(random.randint(100000000000, 999999999999)), mobile_number='+' + str(random.randint(100000000000, 999999999999)),
+                     birth_day=random.randint(1, 31), birth_month=list_of_month[random.randint(0, 12)], birth_year=random.randint(1950, 2021))]
+
+
+@pytest.mark.parametrize("contact", test_data, ids=[repr(x) for x in test_data])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(first_name="Test_name", middle_name="Test_middle_name", last_name="Test_last_name",
-                      address="asdasdasd", home_number="123", mobile_number="131231231",
-                      birth_day="3", birth_month="March", birth_year="1996")
     app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,9 +1,20 @@
+import pytest
 from model.group import Group
+import random
+import string
 
 
-def test_add_group(app):  # Тестовый метод, принимающий фикстуру в качестве параметра
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*7
+    return prefix + "".join([random.choice(symbols) for _ in range(random.randrange(maxlen))])
+
+
+testdata = [Group(name=random_string("name", 10), header=random_string("header", 15), footer=random_string("footer", 20))]
+
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):  # Тестовый метод, принимающий фикстуру в качестве параметра
     old_groups = app.group.get_group_list()
-    group = Group(name="test3", header="dsffsd", footer="sggsdgs")
     app.group.create(group)  # всп метод
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
