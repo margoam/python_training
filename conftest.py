bd892cdf61e135137a864894013bfc89f6c38ea6
margoam <mozheliukrita@gmail.com>
2021-10-26 21:40:49 +0300

    Добавлена передача параметров в тесты из командной строки (логин и пароль)

--- conftest.py
+++ conftest.py
@@ -9,12 +9,14 @@ def app(request):
     global fixture
     browser = request.config.getoption("--browser")
     base_url = request.config.getoption("--baseUrl")
+    username = request.config.getoption("--username")
+    password = request.config.getoption("--password")
     if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)  # Инициализация фикстуры
+        fixture = Application(browser=browser, base_url=base_url, username=username, password=password)  # Инициализация фикстуры
     else:
         if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)  # Инициализация фикстуры
-    fixture.session.ensure_login(username="admin", password="secret")
+            fixture = Application(browser=browser, base_url=base_url, username=username, password=password)  # Инициализация фикстуры
+    fixture.session.ensure_login(username, password)
     return fixture
 
 
@@ -30,3 +32,5 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/index.php")
+    parser.addoption("--username", action="store")
+    parser.addoption("--password", action="store")
--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,7 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self, browser, base_url):
+    def __init__(self, browser, base_url, username, password):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
@@ -22,6 +22,8 @@ class Application:
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
         self.base_url = base_url
+        self.username = username
+        self.password = password
 
     def is_valid(self):  # блок с перехватом исключений
         try:
