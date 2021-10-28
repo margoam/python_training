e94325f2995c89984506f0d655732458f8b17f03
margoam <mozheliukrita@gmail.com>
2021-10-28 20:07:45 +0300

    Исправление пределов в функции randint()

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -15,7 +15,7 @@ list_of_month = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
 test_data = [Contact(first_name=random_string("first_name", 10), middle_name=random_string("middle_name", 10),
                      last_name=random_string("last_name", 10), address=random_string("address", 20),
                      home_number='+' + str(random.randint(100000000000, 999999999999)), mobile_number='+' + str(random.randint(100000000000, 999999999999)),
-                     birth_day=random.randint(1, 31), birth_month=list_of_month[random.randint(0, 12)], birth_year=random.randint(1950, 2021),
+                     birth_day=random.randint(1, 31), birth_month=list_of_month[random.randint(0, 11)], birth_year=random.randint(1950, 2021),
                      second_number='+' + str(random.randint(100000000000, 999999999999)),
                      work_number='+' + str(random.randint(100000000000, 999999999999))) for i in range(3)]
 
