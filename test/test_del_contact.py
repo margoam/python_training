4ba0ba10d38871b578b9133e9bb9f55826afccf1
margoam <mozheliukrita@gmail.com>
2021-11-10 19:25:45 +0300

    Реализация сверки списков из БД для теста модификации контактов (убрана лишняя проверка

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -13,7 +13,6 @@ def test_del_contact(app, db, check_ui):
     app.contact.delete_contact_by_id(contact.id)
     time.sleep(1)
     new_contacts = db.get_contact_list()
-    assert len(old_contacts) - 1 == len(new_contacts)
     old_contacts.remove(contact)
     assert sorted(new_contacts, key=Contact.id_or_max) == sorted(old_contacts, key=Contact.id_or_max)
     if check_ui:
