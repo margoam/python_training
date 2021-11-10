7ba45a1d2bfec55c2fb9050f545ce5c1be916804
margoam <mozheliukrita@gmail.com>
2021-11-10 18:20:45 +0300

    Реализация сверки списков из БД для теста модификации групп

--- test/test_modif_group.py
+++ test/test_modif_group.py
@@ -8,9 +8,11 @@ def test_edit_group(app, db, check_ui):  # Тестовый метод, прин
     old_groups = db.get_group_list()
     group = Group(name="edit_test3")
     groups = random.choice(old_groups)
+    old_groups.remove(groups)
     group.id = groups.id
     app.group.edit_random_group_by_id(groups.id, group)
     new_groups = db.get_group_list()
-    assert len(old_groups) == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     if check_ui:
         assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
